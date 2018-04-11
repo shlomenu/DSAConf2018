721eeb6bd61d5c250b16a016ffda2661340dd3e5
eliw5 <eliw5@DESKTOP-N4E5ATU.wirelessusers1.resnet.slc.edu>
2018-04-11 09:49:21 -0400

    concurrentQueue

---
+++ DSAConf2018/src/concurrentQueue/EmptyException.java
@@ -0,0 +1,10 @@
+package concurrentQueue;
+
+public class EmptyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ DSAConf2018/src/concurrentQueue/LocklessQueue.java
@@ -0,0 +1,67 @@
+package concurrentQueue;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class LocklessQueue<T> {
+
+	AtomicReference<Node> tail, head;
+	
+	public LocklessQueue() {
+		tail = new AtomicReference<Node>();
+		head = tail;
+	}
+	
+	private class Node {
+		public T value;
+		public AtomicReference<Node> next;
+		public Node(T _value) {
+			_value = value;
+			next = new AtomicReference<Node>(null);
+		}
+	}
+	public void enqueue(T value) {
+		Node node = new Node(value);
+		while (true) {
+			Node last = tail.get();
+			Node next = last.next.get();
+			if (last == tail.get()) {
+				if (next == null) {
+					if (last.next.compareAndSet(next, node)) {
+						tail.compareAndSet(last, node);
+						return;
+					}
+				} else {
+					tail.compareAndSet(last, next);
+				}
+	
+			}
+			
+		
+		}
+	}
+	
+	public T dequeue() throws EmptyException {
+		while (true) {
+			Node _head = head.get();
+			Node _tail = tail.get();
+			Node next = _head.next.get();
+			if (_head == head.get()) {
+				if (_head == _tail) {
+					if (next == null) {
+						throw new EmptyException();
+					}
+				}
+				tail.compareAndSet(_tail, next);
+			} else {
+				T _value = next.value;
+				if (head.compareAndSet(_head, next)) {
+					return _value;
+				}
+				
+			}
+		}
+	}
+	
+}
+	
+
