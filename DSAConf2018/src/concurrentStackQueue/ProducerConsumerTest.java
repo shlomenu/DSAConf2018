f8e0e797cc3e6aef223f7262d385cb2c6c823726
eliw5 <eliw5@DESKTOP-N4E5ATU.wirelessusers1.resnet.slc.edu>
2018-04-25 10:56:42 -0400

    Creating test

--- DSAConf2018/src/concurrentStackQueue/ProducerConsumerTest.java
+++ DSAConf2018/src/concurrentStackQueue/ProducerConsumerTest.java
@@ -3,6 +3,33 @@ package concurrentStackQueue;
 public class ProducerConsumerTest {
 	
 	public void main(String[] args) {
+		Thread[] threads = new Thread[5];
 		
+		for (int i = 0; i < threads.length; i++) {
+			if (i % 2 == 1) {
+				threads[i] = new QueueTest();
+			} else {
+				threads[i] = new StackTest();
+			}
+		}
+		
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
 	}
+	
+	private class QueueTest extends Thread {
+		static LocklessQueue queue = new LocklessQueue<Integer>();
+		public void run() {
+			
+		}
+	}
+	
+	private class StackTest extends Thread {
+		
+		public void run() {
+			
+		}
+	}
+	
 }
